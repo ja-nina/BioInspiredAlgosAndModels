a1755808f880bb6f7b390a51777d3e3487ab96c2
Stachowiak, Bartosz <bartosz.stachowiak@aptiv.com>
2024-04-19 17:02:44 +0200

    add ATSP tests

--- atsp_solver/src/atsp.rs
+++ atsp_solver/src/atsp.rs
@@ -116,3 +116,55 @@ impl ATSP {
         cost
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const DATA_PATH: &str = "../data/ALL_atsp/";
+
+    #[test]
+    fn read_from_file() {
+        let atsp = ATSP::read_from_file(&format!("{}/ftv170.atsp", DATA_PATH)).unwrap();
+        assert_eq!(atsp.name, "ftv170");
+        assert_eq!(atsp.comment, "Asymmetric TSP (Fischetti)");
+        assert_eq!(atsp.dimension, 171);
+        assert_eq!(atsp.edge_weight_type, "EXPLICIT");
+        assert_eq!(atsp.edge_weight_format, "FULL_MATRIX");
+        assert_eq!(atsp.matrix.len(), 171);
+        assert_eq!(atsp.matrix[0].len(), 171);
+    }
+
+    #[test]
+    fn cost_of_solution_small_instance() {
+        let atsp = ATSP::read_from_file(&format!("{}/br17.atsp", DATA_PATH)).unwrap();
+        let solution = Solution {
+            dimension: 17,
+            order: (0..17).collect(),
+        };
+        assert_eq!(atsp.cost_of_solution(&solution), 167);
+    }
+
+    #[test]
+    fn cost_of_solution_medium_instance() {
+        let atsp = ATSP::read_from_file(&format!("{}/p43.atsp", DATA_PATH)).unwrap();
+        let solution = Solution {
+            dimension: 43,
+            order: (0..43).collect(),
+        };
+        assert_eq!(atsp.cost_of_solution(&solution), 6160);
+    }
+
+    #[test]
+    fn cost_of_solution_medium_instance_custom_order() {
+        let atsp = ATSP::read_from_file(&format!("{}/p43.atsp", DATA_PATH)).unwrap();
+        let solution = Solution {
+            dimension: 43,
+            order: vec![
+                2, 1, 36, 37, 39, 38, 40, 42, 41, 26, 24, 25, 21, 22, 23, 35, 0, 4, 15, 17, 20, 18,
+                19, 16, 14, 12, 13, 33, 34, 32, 31, 9, 10, 11, 8, 7, 6, 5, 30, 29, 28, 27, 3,
+            ],
+        };
+        assert_eq!(atsp.cost_of_solution(&solution), 5620);
+    }
+}
